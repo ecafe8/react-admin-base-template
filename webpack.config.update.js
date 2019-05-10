b0064e14f5944a107926a534e14004638e4f66a8
zhang <zhangbin@sinxia.com>
2019-05-10 19:55:34 +0800

    update

--- webpack.config.update.js
+++ webpack.config.update.js
@@ -1,15 +1,24 @@
 const path = require('path'); // eslint-disable-line
+const webpack = require('webpack'); // eslint-disable-line
+const IS_DEV = process.env.NODE_ENV !== 'production';
 
 const currentPath = path.resolve(process.cwd(), './src');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line
 
 module.exports = (config) => {  // eslint-disable-line
-  if (process.env.NODE_ENV !== 'production') {
+  if (IS_DEV) {
     config.entry = {
       ...config.entry,
       mock: currentPath + '/mock/index.js',
     };
+  } else {
+    const LimitChunkCountPlugin = new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 3, // 必须大于或等于 1
+      // minChunkSize: 10000
+    });
+    config.plugins = config.plugins.concat(LimitChunkCountPlugin);
   }
+
 
 
   config.resolve = {
